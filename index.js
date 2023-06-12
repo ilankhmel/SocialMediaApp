f0144c8371af7b8d3c51110d7224cf1dc0968fc2
Ilan Khmelnitsky <ilankhmel@gmail.com>
2023-06-12 19:17:53 +0300

    login screen stylr fix

--- index.js
+++ index.js
@@ -1,61 +1,64 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
 
-const mongoose = require("mongoose")
-const dotenv = require("dotenv")
-const helmet = require("helmet")
-const morgan = require("morgan")
-const cors = require('cors')
-const userRoute = require('./routes/user')
-const authRoute = require('./routes/auth')
-const postRoute = require('./routes/post')
-const conversationRoute = require('./routes/conversation')
-const messageRoute = require('./routes/message')
-const { setupSocketAPI } = require("./socket")
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const cors = require("cors");
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+const postRoute = require("./routes/post");
+const conversationRoute = require("./routes/conversation");
+const messageRoute = require("./routes/message");
+const { setupSocketAPI } = require("./socket");
 
-const http = require('http').createServer(app)
+const http = require("http").createServer(app);
 
-setupSocketAPI(http)
+setupSocketAPI(http);
 
-dotenv.config()
+dotenv.config();
 
-try{
-    mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true },
-        () => {
-            console.log("Connected to MongoDB");
-        });
-}catch(err){
-    console.log('ERROR');
-    console.log(err);
+try {
+  mongoose.connect(
+    process.env.MONGO_URL,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    () => {
+      console.log("Connected to MongoDB");
+    }
+  );
+} catch (err) {
+  console.log("ERROR");
+  console.log(err);
 }
 
 //Middleware
-app.use(express.json())
+app.use(express.json());
 // app.use(helmet())
-app.use(morgan())
+app.use(morgan());
+app.use(cookieParser());
 
-if (process.env.NODE_ENV === 'production') {
-    // app.use(express.static(path.resolve(__dirname, 'public')))
-    app.use(express.static('public'))
+if (process.env.NODE_ENV === "production") {
+  // app.use(express.static(path.resolve(__dirname, 'public')))
+  app.use(express.static("public"));
 } else {
-    const corsOptions = {
-        origin: ['http://127.0.0.1:3000', 'http://localhost:3000'],
-        credentials: true 
-    }
-    app.use(cors(corsOptions))
+  const corsOptions = {
+    origin: ["http://127.0.0.1:3000", "http://localhost:3000"],
+    credentials: true,
+  };
+  app.use(cors(corsOptions));
 }
 
-app.use("/api/user", userRoute)
-app.use("/api/auth", authRoute)
-app.use("/api/post", postRoute)
-app.use("/api/conversation", conversationRoute)
-app.use("/api/message", messageRoute)
+app.use("/api/user", userRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/post", postRoute);
+app.use("/api/conversation", conversationRoute);
+app.use("/api/message", messageRoute);
 
-const port = process.env.PORT || 8800
-
-
-
+const port = process.env.PORT || 8800;
 
 http.listen(port, () => {
-    console.log(`Backend server is running at port ${port}`);
-})
+  console.log(`Backend server is running at port ${port}`);
+});
--- routes/auth.js
+++ routes/auth.js
@@ -1,52 +1,75 @@
-const router = require('express').Router()
-const User = require('../models/User')
-const bcrypt = require("bcrypt")
+const router = require("express").Router();
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const secretKey = "secret";
+
 //REGISTER
 
-router.post("/register", async (req, res)=>{    
-    try{
-        //Generate hashed password
-        const salt = await bcrypt.genSalt(10)
-        const hashedPassword = await bcrypt.hash(req.body.password, salt) 
+router.post("/register", async (req, res) => {
+  try {
+    //Generate hashed password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
-        //New user
-        const newuser = await new User({
-            username: req.body.username,
-            email: req.body.email,
-            password: hashedPassword,
-            profilePicture: req.body.profilePicture,
-            coverPicture: req.body.coverPicture,
-            followers: req.body.followers,
-            following: req.body.following,
-        })
-        console.log(newuser);
-        //Save user and respond
-        const user = await newuser.save()
-        res.status(200).json(user)
-        
-    }catch(err){
-        res.status(500).json(err)
-        console.log(err);
-    }
+    //New user
+    const newuser = await new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword,
+      profilePicture: req.body.profilePicture,
+      coverPicture: req.body.coverPicture,
+      followers: req.body.followers,
+      following: req.body.following,
+    });
+    console.log(newuser);
+    //Save user and respond
+    const user = await newuser.save();
 
-})
+    const token = jwt.sign({ userId: user._id }, secretKey);
+    res.cookie("token", token);
+
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json(err);
+    console.log(err);
+  }
+});
 
 //LOGIN
 
-router.post("/login", async(req, res)=>{
-    try{
-        const user = await User.findOne({email: req.body.email})
-        if(!user){
-           return res.status(404).send("user not found") 
-        } 
-        const validPassword = await bcrypt.compare(req.body.password, user?.password)
-        !validPassword && res.status(400).json("wrong password")
-        res.status(200).json(user)
-    }catch(err){
-        console.log(err);
-        res.status(500).json('Account not found')
+router.post("/login", async (req, res) => {
+  try {
+    if (req.cookies.token) {
+      //   console.log(req.cookies.token);
+      const decoded = jwt.verify(req.cookies.token, secretKey);
+      const { userId } = decoded;
+      const user = await User.findById(userId);
+      if (user) return res.status(200).json(user);
     }
 
-})
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      return res.status(404).send("user not found");
+    }
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user?.password
+    );
+    !validPassword && res.status(400).json("wrong password");
 
-module.exports = router
+    const token = jwt.sign({ userId: user._id }, secretKey);
+    res.cookie("token", token);
+    // const salt = await bcrypt.genSalt(10);
+
+    // const token = await bcrypt.hash(user, salt);
+
+    res.status(200).json(user);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json("Account not found");
+  }
+});
+
+module.exports = router;
